7bcc4e259f6d6075a82c14d13bbcb98cc092f835
makyox <makyox@gmail.com>
2021-01-16 20:01:23 +0100

    initial commit

---
+++ frontity.settings.js
@@ -0,0 +1,49 @@
+const settings = {
+  "name": "my-app",
+  "state": {
+    "frontity": {
+      "url": "https://test.frontity.org",
+      "title": "Test Frontity Blog",
+      "description": "WordPress installation for Frontity development"
+    }
+  },
+  "packages": [
+    {
+      "name": "@frontity/mars-theme",
+      "state": {
+        "theme": {
+          "menu": [
+            [
+              "Home",
+              "/"
+            ],
+            [
+              "Blog",
+              "/category/blog/"
+            ],
+            [
+              "Podstrona",
+              "/test/"
+            ]
+          ],
+          "featured": {
+            "showOnList": false,
+            "showOnPost": true
+          }
+        }
+      }
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "url": "http://wp"
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
